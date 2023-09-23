13032d18fed92fefba9d91810c1c9f638d3fa0e9
fabio <fabio@gmail.com>
2023-09-22 21:46:35 -0300

    first commit

---
+++ Clase 1/TiposDeDatos.js
@@ -0,0 +1,27 @@
+//primitives
+typeof undefined // undefined
+typeof true // boolean
+typeof 1 // number
+typeof "string" // string
+typeof 2n // bigint
+typeof Symbol() // symbol
+
+typeof null // null*
+
+const nombre = "fabio"
+const segundoNombre = "fabio"
+nombre === segundoNombre
+"fabio" === "fabio"
+1 === 1
+
+;({a:1} === {a:1})
+;([1,2,3] === [1,2,3])
+
+//objects
+typeof {}
+typeof []
+const set = new Set()
+typeof set
+
+//function
+typeof function(){}
